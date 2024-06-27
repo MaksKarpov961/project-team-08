b604fb038be2d98e818f766f6bd79b59da8e4741
Vasyl <the3akoh@gmail.com>
2024-06-27 15:32:26 +0300

    reviews

---
+++ src/js/swiper.js
@@ -0,0 +1,34 @@
+new Swiper('.swiper', {
+    // кількість слайдів 
+    slidesPerView: 1,
+  
+    loop: true,
+  
+    // відступ 
+    spaceBetween: 16,
+  
+    autoplay: {
+      delay: 3000,
+    },
+  
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // Брейк-поінти
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1280: {
+        slidesPerView: 3,
+      },
+    },
+  });
