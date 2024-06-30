f2e500ae6a1ccdc36833620c50ea7d4dfa5be36b
Maksim Karpenko <ozuzuandvduvatel@gmail.com>
2024-06-30 19:57:09 +0300

    update-check

--- src/js/open-menu.js
+++ src/js/open-menu.js
@@ -1,14 +1,43 @@
 (() => {
-  const refs = {
-    openMobileBtn: document.querySelector('[open-mobile-menu]'),
-    closeMobileBtn: document.querySelector('[close-mobile-menu]'),
-    mmenu: document.querySelector('[mobile-menu-container]'),
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const anchors = mobileMenu.querySelectorAll('a[href*="#"]');
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+    
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+    
+    if(anchors.length === 0) return;
+    
+    if(!isMenuOpen) {
+      anchors.forEach(anchor => {
+        anchor.addEventListener("click", toggleMenu)
+      })
+      return;
+    }
+    
+    anchors.forEach(anchor => {
+        anchor.removeEventListener("click", toggleMenu)
+      })
   };
 
-  refs.openMobileBtn.addEventListener('click', toggleMobile);
-  refs.closeMobileBtn.addEventListener('click', toggleMobile);
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
 
-  function toggleMobile() {
-    refs.mmenu.classList.toggle('is-open');
-  }
+  // Вказати брейкпоінт після якого повинна зачинятися
+  window.matchMedia('(min-width: 375px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
 })();
